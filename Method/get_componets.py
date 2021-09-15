1a9150436f91908a8d6d96b1d59fdb6d253149f0
sauldnn <71799467+sauldnn@users.noreply.github.com>
2021-09-14 23:00:45 -0500

    Add files via upload

--- Method/get_componets.py
+++ Method/get_componets.py
@@ -1,33 +1,29 @@
-def get_components():
-    """
-    Se construye la matriz de unos y se remplazan el primer bloque
-    con la matriz de distancias, se calculan los componenetes de la ecuación
-    AX=B.
+def get_components(ubic, U):
+  """
+  Input:
+    ubic: List
+      Lista de dimensión (2,1) para el cual se va a calcular el variograma
+    U: numpy.ndarray
+      Arreglo que contiene los datos del muestreo necesarios para construir el variograma de ubic.
+  Output:
+    matr: numpy.ndarray
+      Matriz de dimensión (nkrig+1, nkrig+1) que contiene los coeficientes del sistema de ecuaciones
+      creado por el variograma
+    equ: numpy.ndarray
+      Vector de dimensión (1, nkrig+1) que contiene el RHS de la ecuación Ax=b
+    to_mult: numpy.ndarray
+      Vector de estimadores de dimensión (1, nkrig)
 
-    Parameters
-    ----------
-    U : List
-        Lista de dos dimensiones [[], ..., []] que contiene el sampleo
-
-    ubic : List
-        Lista de posicion ['fecha', 0] en la cuál se va a calcular el valor del parametro.
-
-    Returns
-    -------
-    matr, equ, to_mult: lists
-        Componentes de la ecuación AX=B
-
-    """
-    dim = len(U)
-    U.append(ubic)
-    #Distances = [[torch.dist(torch.tensor(U[i]), torch.tensor(U[j]), 2).item() for i in range(dim+1)] for j in range(dim+1)]
-    Distances = [[float(np.linalg.norm((U[i][0]-U[j][0],U[i][1]-U[j][1]))) for i in range(dim+1)] for j in range(dim+1)]
-    S = np.array([[1.0 for i in range(dim+2)] for j in range(dim+1)])
-    D = np.array(Distances)
-    S[0:dim, 0:dim] = D[:dim,:dim]
-    S[dim, dim] = 0.0
-    S[0:dim, dim+1] = D[0:dim, dim]
-    matr = S[:dim+1, :dim+1]
-    equ = S[0:dim+1, dim+1]
-    to_mult = S[:dim, dim+1]
-    return matr, equ, to_mult
+  """
+  dim = len(U)
+  U.append(ubic)
+  Distances = distance_matrix(U, U).tolist()
+  S = np.ones((dim+1, dim+2))
+  D = np.array(Distances)
+  S[0:dim, 0:dim] = D[:dim,:dim]
+  S[dim, dim] = 0.0
+  S[0:dim, dim+1] = D[0:dim, dim]
+  matr = S[:dim+1, :dim+1]
+  equ = S[0:dim+1, dim+1]
+  to_mult = S[:dim, dim+1]
+  return matr, equ, to_mult
